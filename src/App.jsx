1343952064ecbd2056dbb3fb422142b12062971d
joao magnago <joaomagnago.wm@gmail.com>
2023-09-28 20:26:22 -0300

    refactor: create post props and properly use the component in app

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,37 @@ import styles from "./App.module.css";
 import { Sidebar } from "./components/Sidebar";
 import "./global.css";
 
+const posts = [
+  {
+    id: '1',
+    author: {
+      avatarUrl: "https://github.com/JoaoMagnago.png",
+      name: 'João Magnago',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa.É um projeto que fiz no NLW Return, evento da Rocketseat.O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design / doctorcare' },
+    ],
+    publishedAt: new Date('2023-09-25 20:00:00'),
+  },
+  {
+    id: '2',
+    author: {
+      avatarUrl: "https://github.com/maykbrito.png",
+      name: 'Mayk Brito',
+      role: 'Educator'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa.É um projeto que fiz no NLW Return, evento da Rocketseat.O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design / doctorcare' },
+    ],
+    publishedAt: new Date('2022-09-27 20:00:00'),
+  },
+]
+
 export function App() {
   return (
     <div>
@@ -14,6 +45,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
+          {posts.map(post => (
+            <Post
+              key={post.id}
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          ))}
+
           <Post
             author="João Magnago"
             content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam accusantium illum delectus beatae, nihil maxime iure tempore velit mollitia facilis quos, libero ex reiciendis. Reprehenderit nihil iste laborum amet delectus!"
